0baeac8187f77fd18b4d8a75e182d7aa97a1ce27
Hugo Ulloa Ramirez <hugo.ulloa@wizeline.com>
2024-07-15 13:15:28 -0600

    Secret credentials added in login test

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -13,7 +13,7 @@ test('Verify Standard User can login to the application successfully', async ({ 
 test('Verify Locked out user can login to the application', async ({ page }) => {
     await page.goto(URLS.SAUCEDEMOURL);
     const Login = new LoginPage(page);
-    await Login.login("locked_out_user", CREDENTIALS.SAUCEDEMOPASS);
+    await Login.login(CREDENTIALS.SAUCEDEMOLOCKEDUSER, CREDENTIALS.SAUCEDEMOPASS);
     await Login.verifyLockedUserErrorMessage();
 
 });
@@ -22,7 +22,7 @@ test("Verify problem user can login to the application", async ({ page }) => {
     await page.goto(URLS.SAUCEDEMOURL);
     const Login = new LoginPage(page);
 
-    await Login.login("problem_user", CREDENTIALS.SAUCEDEMOPASS);
+    await Login.login(CREDENTIALS.SAUCEDEMOPROBLEMUSER, CREDENTIALS.SAUCEDEMOPASS);
     await Login.verifyUserGotoHomePage();
 
 });
@@ -31,7 +31,7 @@ test('Verify performance_glitch_user can login to the application', async ({ pag
     await page.goto(URLS.SAUCEDEMOURL);
     const Login = new LoginPage(page);
 
-    await Login.login('performance_glitch_user', CREDENTIALS.SAUCEDEMOPASS);
+    await Login.login( CREDENTIALS.SAUCEDEMOGLITCH, CREDENTIALS.SAUCEDEMOPASS);
     await Login.verifyUserGotoHomePage();
 });
 
@@ -48,7 +48,7 @@ test('Verify user cannot login to application with a incorrect user name', async
 test('Verify user cannot login to the application with a incorrect password', async ({ page }) => {
     await page.goto(URLS.SAUCEDEMOURL);
     const Login = new LoginPage(page);
-    Login.login('standard_user', 'pass');
+    Login.login(CREDENTIALS.SAUCEDEMOUSER, 'pass');
 
     await Login.verifyIncorrectUserNameErrorMessage();
 });
